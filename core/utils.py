42400e1f5688134ac6731be01190253c01d25b97
skyduy <cuteuy@gmail.com>
2019-04-03 18:22:25 +0800

    complte data component

--- core/utils.py
+++ core/utils.py
@@ -1,47 +1,102 @@
 import os
-import cv2
+import torch
 import numpy as np
-from core import preprocess
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms import transforms, functional
 
-APPEARED_LETTERS = [
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E',
-    'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'R', 'S', 'T', 'U', 'V', 'W',
-    'X', 'Y', 'Z'
-]
-CAT2CHR = dict(zip(range(len(APPEARED_LETTERS)), APPEARED_LETTERS))
-CHR2CAT = dict(zip(APPEARED_LETTERS, range(len(APPEARED_LETTERS))))
+CHARS = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C',
+         'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'R', 'S',
+         'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
 
-
-def distinct_char(folder):
-    chars = set()
-    for fn in os.listdir(folder):
-        if fn.endswith('.jpg'):
-            for letter in fn.split('.')[0]:
-                chars.add(letter)
-    return sorted(list(chars))
+CAT2CHAR = dict(zip(range(len(CHARS)), CHARS))
+CHAR2CAT = dict(zip(CHARS, range(len(CHARS))))
 
 
-def load_data(folder):
-    img_list = [i for i in os.listdir(folder) if i.endswith('jpg')]
-    letters_num = len(img_list) * 5
-    print('total letters:', letters_num)
-    data = np.empty((letters_num, 40, 40, 3), dtype="uint8")  # channel last
-    label = np.empty((letters_num,))
-    for index, img_name in enumerate(img_list):
-        raw_img = preprocess.load_img(os.path.join(folder, img_name))
-        sub_imgs = preprocess.gen_sub_img(raw_img)
-        for sub_index, img in enumerate(sub_imgs):
-            data[index*5+sub_index, :, :, :] = img / 255
-            label[index*5+sub_index] = CHR2CAT[img_name[sub_index]]
-        if index % 100 == 0:
-            print('{} letters loads'.format(index*5))
-    return data, label
+class ImageDataset(Dataset):
+    def __init__(self, img_folder, train=True, transform=None, split_rate=0.2):
+        src, _ = os.path.split(os.path.abspath(__file__))
+        wd = os.path.dirname(src)
+        self.folder = os.path.join(wd, img_folder)
+        imgs = [i for i in os.listdir(self.folder) if i.endswith('jpg')]
+        if not imgs:
+            raise Exception('Empty folder!')
+
+        point = int(split_rate * len(imgs))
+        if train is True:
+            self.imgs = imgs[point:]
+        else:
+            self.imgs = imgs[:point]
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.imgs)
+
+    def __getitem__(self, idx):
+        labels = [CHAR2CAT[self.imgs[idx][i]] for i in range(5)]
+        path = os.path.join(self.folder, self.imgs[idx])
+        im = Image.open(path)
+        if im.mode != 'RGB':
+            im = im.convert('RGB')
+        sample = {'image': im, 'labels': labels}
+        if self.transform:
+            sample = self.transform(sample)
+        return sample
+
+
+class ToTensor(transforms.ToTensor):
+    def __call__(self, sample):
+        im, labels = sample['image'], sample['labels']
+        np_img = np.asarray(im)
+        np_labels = np.asarray(labels)
+        # swap color axis because
+        # numpy image: H x W x C
+        # torch image: C X H X W
+        image = np_img.transpose((2, 0, 1))
+        return {'image': torch.from_numpy(image).float(),
+                'labels': torch.from_numpy(np_labels)}
+
+
+class Normalize(transforms.Normalize):
+    def __call__(self, sample):
+        tensor = sample['image']
+        sample['image'] = functional.normalize(
+            tensor, self.mean, self.std, self.inplace)
+        return sample
+
+
+def load_data():
+    transform = transforms.Compose([
+        ToTensor(),
+        Normalize([127.5, 127.5, 127.5], [128, 128, 128])
+    ])
+
+    trainset = ImageDataset('data', transform=transform)
+    trainloader = DataLoader(trainset, batch_size=4, shuffle=True,
+                             num_workers=2)
+
+    testset = ImageDataset('data', train=False, transform=transform)
+    testloader = DataLoader(testset, batch_size=4, num_workers=2)
+
+    return trainloader, testloader, CHARS
+
+
+def imshow(img):
+    img = img * 128 + 127.5  # unnormalize
+    npimg = img.numpy()
+    npimg = np.transpose(npimg, (1, 2, 0))
+    im = Image.fromarray(npimg.astype('uint8'))
+    im.show()
 
 
 if __name__ == '__main__':
-    # print(distinct_char('../data'))
-    d, l = load_data('../samples')
-    for n, i in enumerate(d):
-        cv2.imshow(CAT2CHR[l[n]], i*255)
-        print(CAT2CHR[l[n]])
-        cv2.waitKey(0)
+    import torchvision
+
+    trains, tests, classes = load_data()
+    dataiter = iter(trains)
+    data = next(dataiter)
+    images, labels = data['image'], data['labels']
+
+    # show image and print labels
+    imshow(torchvision.utils.make_grid(images))
+    print(labels)
