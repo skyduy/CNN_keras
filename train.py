be0b249a6c743434d19cf72cfad8f10b17bc3ca0
skyduy <cuteuy@gmail.com>
2019-04-04 22:29:03 +0800

    simplify model

--- train.py
+++ train.py
@@ -25,24 +25,24 @@ class Net(MyModule):
     def __init__(self, gpu=False):
         super(Net, self).__init__()
         # size: 3 * 40 * 150
-        self.conv1 = nn.Conv2d(3, 32, 9)  # 32 * 32 * 142
-        self.conv2 = nn.Conv2d(32, 32, 9)  # 32 * 24 * 134
-        self.pool = nn.MaxPool2d(2)  # 32 * 12 * 67
-        self.drop1 = nn.Dropout2d(0.25)
+        self.conv1 = nn.Conv2d(3, 32, (5, 9))  # 32 * 36 * 142
+        self.pool1 = nn.MaxPool2d((2, 4))  # 32 * 18 * 35
+        self.conv2 = nn.Conv2d(32, 16, (5, 7))  # 16 * 14 * 29
+        self.pool2 = nn.MaxPool2d((2, 3))  # 16 * 7 * 9
         # flatten here
-        self.fc1 = nn.Linear(32 * 12 * 67, 640 * 5)
-        self.drop2 = nn.Dropout(0.5)
-        self.fc2 = nn.Linear(640 * 5, 33 * 5)
+        self.fc = nn.Linear(16 * 7 * 9, 33 * 5)
+        self.sm = nn.Softmax(1)
+
         if gpu:
             self.to(DEVICE)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
+        x = self.pool1(x)
         x = F.relu(self.conv2(x))
-        x = self.drop1(self.pool(x))
-        x = x.view(-1, 32 * 12 * 67)
-        x = self.drop2(self.fc1(x))
-        x = self.fc2(x)
+        x = self.pool2(x)
+        x = x.view(-1, 16 * 7 * 9)
+        x = self.sm(self.fc(x))
         return x
 
 
@@ -81,9 +81,9 @@ def loss_batch(model, loss_func, data, opt=None):
     loss = loss_func(out, yb)
 
     if opt is not None:
+        opt.zero_grad()
         loss.backward()
         opt.step()
-        opt.zero_grad()
 
     loss_item = loss.item()
     del out
@@ -109,9 +109,9 @@ def fit(epochs, model, loss_func, opt, train_dl, valid_dl):
 def train(use_gpu=True):
     train_dl, valid_dl = load_data(
         batch_size=4, max_m=4 * 9, split_rate=0.2, gpu=use_gpu)
-    model = Net2(use_gpu)
+    model = Net(use_gpu)
     opt = optim.Adadelta(model.parameters())
-    criterion = nn.MultiLabelSoftMarginLoss()  # loss function
+    criterion = nn.BCELoss()  # loss function
     fit(100, model, criterion, opt, train_dl, valid_dl)
     model.save('model-{}'.format(datetime.now().strftime("%Y%m%d%H%M%S")))
     print('Training finish')
